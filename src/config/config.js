0a12b8184825450ee890fefb720b97d953f8466d
Jairo Ortiz <jairo.ortiz@4all.com>
2021-05-18 21:23:45 -0300

    feat(docker-compose): Config

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,13 @@
-require('dotenv').config()
+require("dotenv").config();
+
 module.exports = {
-  dialect: 'mysql',
-  host: process.env.NODE_ENV === 'dockerDev' ? process.env.HDOCKER : process.env.HOST,
-  username: process.env.USER,
-  password: process.env.NODE_ENV === 'dockerDev' ? process.env.PASSWORD : process.env.PASSWORD_LOCAL,
-  database: process.env.DATABASE,
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   define: {
-    timestamps: true
+    timestamps: true,
   },
-}
+};
